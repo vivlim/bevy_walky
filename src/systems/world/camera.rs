f01bc6225db047afdb95a2c3b0c51297db09124a
Viv Lim <git@vvn.space>
2024-01-13 03:39:56 -0800

    add gizmo showing camera position

--- src/systems/world/camera.rs
+++ src/systems/world/camera.rs
@@ -28,27 +28,37 @@ pub fn update_camera(
         Without<OrbitCameraTarget>,
     )>,
     targets: Query<(&OrbitCameraTarget, &Transform, Without<LookTransform>)>,
+    mut gizmos: Gizmos,
 ) {
     let mut last_target: Option<&OrbitCameraTarget> = None;
     for (mut unreal_camera, mut look_transform, mut camera_transform, _) in cameras.iter_mut() {
         for (target, target_transform, _) in targets.iter() {
+            // Get camera target yaw and pitch, and compute vector
+            let xz_len = f32::cos(target.pitch);
+            let direction = Vec3::new(
+                xz_len * f32::cos(target.yaw),
+                f32::sin(target.pitch),
+                xz_len * f32::sin(target.yaw * -1.0),
+            );
+            // Multiply it by the desired distance and add it to the target's position.
+            let camera_target_position =
+                (direction * target.distance) + target_transform.translation;
+
             if target.active {
                 unreal_camera.enabled = false;
                 look_transform.target = target_transform.translation;
-                // Get camera target yaw and pitch, and compute vector
-                let xz_len = f32::cos(target.pitch);
-                let direction = Vec3::new(
-                    xz_len * f32::cos(target.yaw),
-                    f32::sin(target.pitch),
-                    xz_len * f32::sin(target.yaw * -1.0),
-                );
-                // Multiply it by the desired distance and add it to the target's position.
-                let camera_target_position =
-                    (direction * target.distance) + target_transform.translation;
                 // Move the camera there.
                 camera_transform.translation = camera_target_position;
                 return;
             }
+
+            // It's not active, let's draw a gizmo so we can examine it.
+            gizmos.ray(
+                target_transform.translation,
+                direction * target.distance,
+                Color::BLUE,
+            );
+            gizmos.sphere(camera_target_position, Quat::default(), 0.5, Color::BLUE);
         }
 
         unreal_camera.enabled = true;
